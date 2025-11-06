84db4dfce9e32c50b39782e44cf47151989730e2
gusandrini <gustavosandrini92@gmail.com>
2025-11-06 16:15:54 -0300

    ajustando

--- src/test/java/br/com/fiap/projeto_mottu/repository/PaisRepositoryTest.java
+++ src/test/java/br/com/fiap/projeto_mottu/repository/PaisRepositoryTest.java
@@ -9,13 +9,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import br.com.fiap.projeto_mottu.model.Pais;
 
 @DataJpaTest
-@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @TestPropertySource(locations = "classpath:application-test.properties")
 public class PaisRepositoryTest {
 
